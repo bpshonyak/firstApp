22c8729bb45e2c9b1c28077e150436d4a37f86d2
Bogdan Pshonyak <bpshonyak@live.com>
2016-11-09 13:55:12 -0800

    completed simple gulp setup

--- gulpfile.js
+++ gulpfile.js
@@ -2,21 +2,35 @@ const gulp = require('gulp');
 const pump = require('pump');
 const concat = require('gulp-concat');
 const babel = require('gulp-babel');
+const sass = require('gulp-sass');
 const sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('default', defaultTask);
+gulp.task('scssTask', scssTask);
+gulp.task('scriptTask', scriptTask);
+gulp.task('default', ['scriptTask', 'scssTask']);
 
-function defaultTask(done) {
+function scriptTask(done) {
 	pump([
-    gulp.src([
-      "app/**/*.js"
-    ]),
+    gulp.src('app/**/*.js'),
     sourcemaps.init(),
 		babel({
 			presets: ['es2015']
 		}),
-    concat('index.js'),
+    concat('index.min.js'),
     sourcemaps.write('.'),
     gulp.dest('www/js/')
   ], done);
 };
+
+function scssTask(done) {
+  pump([
+    gulp.src('app/**/*.scss'),
+    sourcemaps.init(),
+    sass({
+      outputStyle: 'compressed'
+    }).on('error', sass.logError),
+    concat('index.min.css'),
+    sourcemaps.write('.'),
+    gulp.dest('www/css/')
+  ], done);
+}
