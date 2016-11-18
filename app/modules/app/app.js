111274aad49d34b0bf1c3209ecf218e427931c3c
Bogdan Pshonyak <bpshonyak@live.com>
2016-11-18 00:46:01 -0800

    added settings

--- app/modules/app/app.js
+++ app/modules/app/app.js
@@ -1,13 +1,13 @@
 require('./app.scss');
 
 let homeView = require('../home/home'),
-    contactsView = require('../contacts/contacts');
-    // settingsView = require('../settings/settings');
+    contactsView = require('../contacts/contacts'),
+    settingsView = require('../settings/settings');
 
 module.exports = {
     init: function() {
         homeView.init();
         contactsView.init();
-        // settingsView.init();
+        settingsView.init();
     }
 };
